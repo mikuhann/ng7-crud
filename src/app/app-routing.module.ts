c5d917819356264e80026b2bcfde5679258a2906
mikuhann <ng.kano@gmail.com>
2019-01-15 21:23:10 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+import { Routes, RouterModule } from '@angular/router';
+// include components for in which router service to be used
+import { AddStudentComponent } from './add-student/add-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+// routes array
+const routes: Routes = [
+  { path: '', redirectTo: '/register-student', pathMatch: 'full' },
+  { path: 'register-student', component: AddStudentComponent },
+  { path: 'view-students', component: StudentListComponent },
+  { path: 'edit-student/:id', component: EditStudentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
