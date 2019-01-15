78ededea9820a0f4398bca94080713971e6c9f2a
mikuhann <ng.kano@gmail.com>
2019-01-15 22:44:19 +0300

    add student component html and define student component

--- src/app/add-student/add-student.component.ts
+++ src/app/add-student/add-student.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../shared/crud.service';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-add-student',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-student.component.css']
 })
 export class AddStudentComponent implements OnInit {
-
-  constructor() { }
+  public studentForm: FormGroup;
+  constructor(
+    public crudApi: CrudService,
+    public fb: FormBuilder,
+    public toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.crudApi.GetStudentsList();
+    this.studenForm();
   }
-
+  studenForm() {
+    this.studentForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: [''],
+      email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]],
+      mobileNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$')]]
+    });
+  }
+  // Accessing form control using getters
+  get firstName() {
+    return this.studentForm.get('firstName');
+  }
+  get lastName() {
+    return this.studentForm.get('lastName');
+  }
+  get email() {
+    return this.studentForm.get('email');
+  }
+  get mobileNumber() {
+    return this.studentForm.get('mobileNumber');
+  }
+  ResetForm() {
+    this.studentForm.reset();
+  }
+  submitStudentData() {
+    this.crudApi.AddStudent(this.studentForm.value);
+    this.toastr.success(this.studentForm.controls['firstName'].value + 'successfully added!');
+    this.ResetForm();
+  }
 }
